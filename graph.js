3c0ecf59884d29d0f319ffa2aae4b4fc2f068120
Dani Huertas <huertas.dani@gmail.com>
2016-05-23 08:14:46 +0000

    Updated LICENSE to GPLv3

--- graph.js
+++ graph.js
@@ -1,6 +1,21 @@
-/*
- * @author dhuertas
- * @email huertas.dani@gmail.com
+/* 
+ * Graph is a library to plot matlab-like charts using HTML 5 canvas
+ * 
+ * Copyright (C) 2016  Daniel Huertas <huertas.dani@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 var Graph = (function() {
